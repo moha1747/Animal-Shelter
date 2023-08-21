4e5e7a76fcafea59073770bc84262e14f49bd152
Abbaas Mohamud <117889391+moha1747@users.noreply.github.com>
2023-08-21 13:43:22 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,13 @@ import Services from './components/services'
 
 import './index.css'
 import './styles/utils.css'
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+
+const script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
+script.async = true;
+script.defer = true;
+document.body.appendChild(script);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
